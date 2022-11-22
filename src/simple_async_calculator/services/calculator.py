bb369813b42215562832a05e2f864e52704f2cd3
Андрей Леонтьев <an.leontiev@cian.ru>
2022-11-22 23:52:29 +0300

    Минимальный функционал вычисления результатов операций

---
+++ src/simple_async_calculator/services/calculator.py
@@ -0,0 +1,18 @@
+from simple_async_calculator.enums.operator import Operator
+
+
+class UnsupportedOperation(Exception):
+    """Исключение для неподдерживаемой операции"""
+
+
+def calculate(*, x: int, y: int, operator: Operator) -> float:
+    """Вычисление результата калькулятора"""
+    match operator:
+        case Operator.SUMMATION:
+            return x + y
+        case Operator.SUBTRACTION:
+            return x - y
+        case Operator.DIVISION:
+            return x / y
+        case _:
+            raise UnsupportedOperation
--- src/simple_async_calculator/services/tasks.py
+++ src/simple_async_calculator/services/tasks.py
@@ -1,19 +1,20 @@
-import logging
-
 from fastapi import FastAPI, Path, status
 
-from simple_async_calculator.entities.create_task import (
+from simple_async_calculator.entities.api.create_task import (
     CreateTaskRequest,
     CreateTaskResponse,
 )
-from simple_async_calculator.entities.task_detail import TaskDetailResponse
-from simple_async_calculator.entities.tasks_listing import TaskItem, TaskListingResponse
+from simple_async_calculator.entities.api.task_detail import TaskDetailResponse
+from simple_async_calculator.entities.api.tasks_listing import TaskItem, TaskListingResponse
+from simple_async_calculator.entities.db.task import Task
 from simple_async_calculator.enums.status import Status
+from simple_async_calculator.services.calculator import calculate
 
 app = FastAPI()
 
-
 MIN_AVAILABLE_TASK_ID: int = 1
+
+TASKS_CONTAINER: list = []
 
 
 @app.post(
@@ -23,14 +24,21 @@ MIN_AVAILABLE_TASK_ID: int = 1
 )
 async def create_task(task: CreateTaskRequest) -> CreateTaskResponse:
     """Сервис ручки создания задачи"""
-    logging.debug(task)
-    return CreateTaskResponse(task_id=1)
+    task_id = len(TASKS_CONTAINER) + 1
+    TASKS_CONTAINER.append(
+        Task(
+            id=task_id,
+            status=Status.PENDING,
+            **task.dict(),
+        )
+    )
+    return CreateTaskResponse(task_id=task_id)
 
 
 @app.get("/tasks", response_model=TaskListingResponse)
 async def tasks_listing() -> TaskListingResponse:
     """Сервис ручки листинга задач"""
-    return TaskListingResponse(tasks=[TaskItem(id=1, status=Status.PENDING)])
+    return TaskListingResponse(tasks=[TaskItem(**task.dict()) for task in TASKS_CONTAINER])
 
 
 @app.get("/tasks/{task_id}", response_model=TaskDetailResponse)
@@ -38,8 +46,9 @@ async def task_detail(
     task_id: int = Path(description="Идентификатор задачи", ge=MIN_AVAILABLE_TASK_ID),
 ) -> TaskDetailResponse:
     """Сервис ручки получения результата задачи"""
-    logging.debug(task_id)
-    return TaskDetailResponse(
-        status=Status.PENDING,
-        result=None,
-    )
+    task: Task = next(filter(lambda tsk: tsk.id == task_id, TASKS_CONTAINER))
+    result = calculate(x=task.x, y=task.y, operator=task.operator)
+    task.result = result
+    task.status = Status.SUCCESS
+    TASKS_CONTAINER[task_id - 1] = task
+    return TaskDetailResponse(**task.dict())
