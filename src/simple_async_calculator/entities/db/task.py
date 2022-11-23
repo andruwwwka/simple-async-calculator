00e6dfc20b28597aeed9dffc18bb546cc1afa59a
Андрей Леонтьев <an.leontiev@cian.ru>
2022-11-23 12:13:46 +0300

    Слой взаимодействия с хранилищем

--- src/simple_async_calculator/entities/db/task.py
+++ src/simple_async_calculator/entities/db/task.py
@@ -4,12 +4,17 @@ from simple_async_calculator.enums.operator import Operator
 from simple_async_calculator.enums.status import Status
 
 
-class Task(BaseModel):
-    """Объект задачи из хранилища"""
+class BaseTaskDB(BaseModel):
+    """Объект задачи для создания сущности в хранилище"""
 
-    id: int
     x: int
     y: int
     operator: Operator
     status: Status
+
+
+class TaskDB(BaseTaskDB):
+    """Объект задачи из хранилища"""
+
+    id: int
     result: float | None = None
